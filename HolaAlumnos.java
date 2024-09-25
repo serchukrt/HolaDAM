158b72e3022fb3c835470599fd729c43c3e18530
Sergio <sergio.rodpri@gmail.com>
2024-09-25 14:14:32 +0200

    Muestra mi nombre completo

--- HolaAlumnos.java
+++ HolaAlumnos.java
@@ -1,6 +1,7 @@
 public class HolaAlumnos {
  
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		System.out.println("Sergio Rodriguez Prieto");		
 		System.out.println("Hola alumnos de DAM1!!");
 	}
  
